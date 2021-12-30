8a180f35b0c2babf323c47fc10199517b173525b
Marin Capan <marin.capan@gmail.com>
2021-12-30 10:34:58 +0100

    added day 1 code of 2021

---
+++ 2021/D-1/2021-1.py
@@ -0,0 +1,18 @@
+import os
+path=os.getcwd()
+L = []
+with open("%s\\2021-1-IN.txt"%(path)) as f:
+    for line in f:
+        L.append(int(line))
+measures={}
+counter=0
+for i in range(0,len(L),1):
+    if (i==0):
+        measures[L[i]]="N/A - no previous measurement"
+    else:
+        if L[i]<L[i-1]:
+            measures[L[i]]="decreased"
+        else:
+            measures[L[i]]="increased"
+            counter+=1
+print(counter)
